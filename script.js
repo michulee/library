55297f13a11b2eb22836c42afa2c979244de8df6
Michelle Lee <michellechulee@gmail.com>
2020-12-29 03:42:44 -0800

    refactored js and css

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 
-let myLibrary = []
+let myLibrary = [];
 const btnAddBook = document.getElementById('addBook');
-
+const bookKey = 'book';
 const cardPos = document.body.querySelector('.grid');
 const btnOpenLibraryForm = document.querySelector('.open');
-const btnCloseLibraryForm = document.querySelector('.close');
+const btnCloseLibraryForm = document.querySelector('.btn-close');
 
 function Book(title, author, read, icon) {
     this.title = title;
@@ -45,10 +45,10 @@ function setModalVisibility(isVisible) {
     }
 }
 
-// Click 'Submit' and create localStorage entry for book item
+// make global variable for 'book'??
 /**
- * 
- * @param {*} e - The target element
+ * Add localStorage entry for book object
+ * @param {*} e - The click event
  */
 function addBookToLibrary(e) {
     if(document.getElementById('title').value === '') {
@@ -59,8 +59,10 @@ function addBookToLibrary(e) {
         book.title = document.getElementById('title').value;
         book.author = document.getElementById('author').value;
         book.setReadStatus(document.getElementById('read').checked);
+
         myLibrary.push(book);
-        localStorage.setItem('book', JSON.stringify(myLibrary));
+        localStorage.setItem(bookKey, JSON.stringify(myLibrary));
+
         displayBookCard();
     }
 }
@@ -84,62 +86,40 @@ function displayBookCard() {
  * Display all books upon loading page
  */
 function displayAllBookCards() {
-    // parseLocalStorage('book');
-    myLibrary = parseLocalStorage('book');
-    myLibrary.forEach(book => {
-        const card = `
-                    <div class="card" data-id='${book.id}'>
-                        <h2>${book.title}</h2>
-                        <div>${book.author}</div>
-                        <div class='read'>
-                            <i class='read-icon material-icons ${book.icon}'>fiber_manual_record</i>
-                            <p class='readText'>${book.read}</p>
-                        </div>
-                        <button class='remove material-icons'>close</button>
-                    </div>`;
-        cardPos.insertAdjacentHTML('afterbegin', card);
-        
-    });
-    // myLibrary.forEach(book => {
-    //     const card = `
-    //                 <div class="card" data-id='${book.id}'>
-    //                     <h2>${book.title}</h2>
-    //                     <div>${book.author}</div>
-    //                     <div class='read'>
-    //                         <i class='read-icon material-icons ${book.icon}'>fiber_manual_record</i>
-    //                         <p class='readText'>${book.read}</p>
-    //                     </div>
-    //                     <button class='remove material-icons'>close</button>
-    //                 </div>`;
-    //     cardPos.insertAdjacentHTML('afterbegin', card);
-        
-    // });
+    myLibrary = parseLocalStorageBook(bookKey);
+    if(myLibrary.length !== 0) {
+        myLibrary.forEach(book => {
+            const card = `
+                        <div class="card" data-id='${book.id}'>
+                            <h2>${book.title}</h2>
+                            <div>${book.author}</div>
+                            <div class='read'>
+                                <i class='read-icon material-icons ${book.icon}'>fiber_manual_record</i>
+                                <p class='readText'>${book.read}</p>
+                            </div>
+                            <button class='remove material-icons'>close</button>
+                        </div>`;
+            cardPos.insertAdjacentHTML('afterbegin', card);
+        });
+    }
 }
 
 /**
  * Parse localStorage objects into myLibrary array
- * @param {*} key - The localStorage key
+ * @param {string} key - The localStorage key
+ * @returns {Array<Object>} Book objects
  */
-// function parseLocalStorage(key) {
-//     const localStorageArray = JSON.parse(localStorage.getItem(key));
-//     if(localStorageArray !== null) {
-//         localStorageArray.forEach(book => {
-//             book = new Book(book.title, book.author, book.read, book.icon);
-//             myLibrary.push(book);
-//         });
-//     }
-// }
-
-function parseLocalStorage(key) {
+function parseLocalStorageBook(key) {
     const localStorageArray = JSON.parse(localStorage.getItem(key));
+    let libraryArray = [];
     if(localStorageArray !== null) {
-        let libraryArray = [];
+        // Set object Book for all book objects from localStorage
         localStorageArray.forEach(book => {
             book = new Book(book.title, book.author, book.read, book.icon);
             libraryArray.push(book);
         });
-        return libraryArray;
     }
+    return libraryArray;
 }
 
 /**
@@ -147,53 +127,64 @@ function parseLocalStorage(key) {
  * read status of a book
  */
 window.addEventListener('click', (e) => {
-    const cardPosition = getTargetElementPosition(e);
-    const cardElement = e.target.parentElement;
-    // Remove the card from display and localStorage when clicking on remove button
-    if(e.target.classList.contains('remove')) {
-        cardElement.remove(); //remove HTML
-        console.log('remove position is ' + cardPosition) // remove entry from myLibrary arr
-        myLibrary.splice(cardPosition, 1)
-        localStorage.setItem('book', JSON.stringify(myLibrary)); // update localStorage entry
-    }
-    // Toggle read status (text and icon) of book
-    else if(cardElement.classList.contains('read')) {
-        const icon = cardElement.querySelector('.read-icon');
-        if(myLibrary[cardPosition].read == 'Already Read') {
-            myLibrary[cardPosition].setReadStatus(false);
-            localStorage.setItem('book', JSON.stringify(myLibrary));
-            if(e.target.classList.contains('readText')) {
-                e.target.innerText = 'Want to Read';
-                icon.classList.remove('icon-green-circle');
-                icon.classList.add('icon-red-circle');
+    const cardPosition = getCardPosition(e);
+    if(cardPosition !== -1) {
+        const cardElement = e.target.parentElement;
+        // Remove the card from display and localStorage when clicking on remove button
+        if(e.target.classList.contains('remove')) {
+            cardElement.remove(); //remove HTML
+    
+            myLibrary.splice(cardPosition, 1); // remove entry from myLibrary arr
+    
+            // update or remove localStorage entry
+            if(myLibrary.length === 0) {
+                localStorage.removeItem(bookKey);
+            } else {
+                localStorage.setItem(bookKey, JSON.stringify(myLibrary)); 
             }
-        } else {
-            myLibrary[cardPosition].setReadStatus(true);
-            localStorage.setItem('book', JSON.stringify(myLibrary));
-            if(e.target.classList.contains('readText')) {
-                e.target.innerText = 'Already Read';
-                icon.classList.remove('icon-red-circle');
-                icon.classList.add('icon-green-circle');
+        }
+        // Toggle read status (text and icon) of book
+        else if(cardElement.classList.contains('read')) {
+            const icon = cardElement.querySelector('.read-icon');
+            const book = myLibrary[cardPosition];
+    
+            if(book.read == 'Already Read') {
+                book.setReadStatus(false);
+                localStorage.setItem(bookKey, JSON.stringify(myLibrary));
+                if(e.target.classList.contains('readText')) {
+                    e.target.innerText = 'Want to Read';
+                    icon.classList.remove('icon-green-circle');
+                    icon.classList.add('icon-red-circle');
+                }
+            } else {
+                book.setReadStatus(true);
+                localStorage.setItem(bookKey, JSON.stringify(myLibrary));
+                if(e.target.classList.contains('readText')) {
+                    e.target.innerText = 'Already Read';
+                    icon.classList.remove('icon-red-circle');
+                    icon.classList.add('icon-green-circle');
+                }
             }
         }
     }
 });
 
 /**
- * Get position of the closest card component starting from the target element
- * @param {*} e - The target element 
+ * Get position of the closest card component
+ * @param {*} e - The click event
+ * @returns {number} Position of card 
  */
-function getTargetElementPosition(e) {
+function getCardPosition(e) {
     const clickedElement = e.target.closest('.card');
+    let cardPosition = -1;
     if(clickedElement !== null) {
         let sibling = clickedElement.nextElementSibling;
-        let cardPosition = 0;
         while(sibling !== null) {
             cardPosition++;
             sibling = sibling.nextElementSibling;
         }
-        return cardPosition;
     }
+    return cardPosition;
 }
 
 
